660ad8fdd35390be21582f00b93ecce545c0aadc
MinaLbc <48332228+MinaLbc@users.noreply.github.com>
2020-04-19 15:59:53 +0200

    Add destination 3 + Add destionation 4

--- Good Boy Wanwan (Ludum Jam)/Assets/Scripts/Curseur.cs
+++ Good Boy Wanwan (Ludum Jam)/Assets/Scripts/Curseur.cs
@@ -6,6 +6,8 @@ public class Curseur : MonoBehaviour
 {
 	public Transform Destination1;
 	public Transform Destination2;
+	public Transform Destination3;
+	public Transform Destination4;
 	public Transform Target;
 
 
@@ -27,7 +29,21 @@ public class Curseur : MonoBehaviour
     void OnTriggerEnter(Collider other){
     	if(other.gameObject.CompareTag("Destination1")){
     		Target = Destination2;
-    		Debug.Log("changement Target");
+    		Debug.Log("changement Target1");
+    	}
+
+    	if(other.gameObject.CompareTag("Destination2")){
+    		Target = Destination3;
+    		Debug.Log("changement Target2");
+    	}
+
+    	if(other.gameObject.CompareTag("Destination3")){
+    		Target = Destination4;
+    		Debug.Log("changement Target3");
+    	}
+
+    	if(other.gameObject.CompareTag("Destination4")){
+       		Debug.Log("You win");
     	}
 
     	
