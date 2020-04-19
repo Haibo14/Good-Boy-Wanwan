0646ccda4746a92a571c16a1a16a38ed329aa5f9
Haibo <haibo@users.noreply.github.com>
2020-04-19 10:06:27 +0100

    Add Main Menu

---
+++ Good Boy Wanwan (Ludum Jam)/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+
+        //Charger la scène de jeu
+        //SceneManager.LoadScene("NameOfTheScene");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+
+}
