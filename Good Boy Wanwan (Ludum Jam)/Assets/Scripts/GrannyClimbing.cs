6325e9662017f130a9147978db7531a0c0174061
Haibo <haibo@users.noreply.github.com>
2020-04-20 13:26:43 +0100

    Add scene next

---
+++ Good Boy Wanwan (Ludum Jam)/Assets/Scripts/GrannyClimbing.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrannyClimbing : MonoBehaviour
+{
+    public GameObject player;
+
+    public float distance = 300f;
+    void FixedUpdate()
+    {
+        Vector3 lookDown = new Vector3(transform.position.x, -10, transform.position.z);
+        RaycastHit hit;
+        
+        if (Physics.Raycast(transform.position, lookDown, out hit, distance))
+        {
+          
+            Vector3 targetLocation = hit.point;
+
+           
+
+            if (targetLocation.y < player.transform.position.y -0.4f && hit.collider.gameObject.CompareTag("Ground"))
+            {
+
+
+                player.transform.Translate(new Vector3(0,0,0.1f));
+                Debug.Log("Ca monte");
+
+            }
+
+            Debug.DrawRay(transform.position, Vector3.down);
+        }
+    }
+}
