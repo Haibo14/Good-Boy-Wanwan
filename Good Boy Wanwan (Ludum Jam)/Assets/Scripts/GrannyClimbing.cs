16baf21e83317cfe096f2e6af864cd37f349fb72
Haibo <haibo@users.noreply.github.com>
2020-04-20 14:32:50 +0100

    Add GrannyClimbing

--- Good Boy Wanwan (Ludum Jam)/Assets/Scripts/GrannyClimbing.cs
+++ Good Boy Wanwan (Ludum Jam)/Assets/Scripts/GrannyClimbing.cs
@@ -10,25 +10,27 @@ public class GrannyClimbing : MonoBehaviour
     void FixedUpdate()
     {
         Vector3 lookDown = new Vector3(0, -10, 0) + player.transform.position;
+        
+        transform.LookAt(lookDown);
         RaycastHit hit;
         
-        if (Physics.Raycast(transform.position, lookDown, out hit, distance))
+        if (Physics.Raycast(transform.position, Vector3.down, out hit, distance))
         {
           
             Vector3 targetLocation = hit.point;
 
-           
+            
 
-            if (targetLocation.y < player.transform.position.y -0.4f && hit.collider.gameObject.CompareTag("Ground"))
+            if (targetLocation.y > player.transform.position.y -0.5f  && hit.collider.gameObject.CompareTag("Ground"))
             {
 
 
-                player.transform.Rotate(new Vector3(0.3f,0,0));
+                player.transform.Rotate(new Vector3(-15f,0,0));
                 Debug.Log("Ca monte");
 
             }
 
-            Debug.DrawRay(transform.position, lookDown);
+            Debug.DrawRay(transform.position, Vector3.down);
         }
     }
 }
